c41486a53c274d92904cbd4628697508dbe8810f
stygigoth <99850233+stygigoth@users.noreply.github.com>
2022-06-08 09:49:22 -0400

    initial commit

---
+++ src/main/java/com/stygigoth/removed/Removed.java
@@ -0,0 +1,15 @@
+package com.stygigoth.removed;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Removed implements ModInitializer {
+    public static final String MOD_ID = "removed";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        
+    }
+}
---
+++ src/main/java/com/stygigoth/removed/RemovedClient.java
@@ -0,0 +1,13 @@
+package com.stygigoth.removed;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class RemovedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
