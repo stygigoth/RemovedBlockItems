157d9377c593c4eb74cbcb6119c984e68dbe28b3
stygigoth <99850233+stygigoth@users.noreply.github.com>
2022-06-09 10:52:21 -0400

    0.3.0-1.19

--- src/main/java/com/stygigoth/removed/ModBlocks.java
+++ src/main/java/com/stygigoth/removed/ModBlocks.java
@@ -8,18 +8,21 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
+    public static Item WATER;
+    public static Item LAVA;
+    public static Item FIRE;
+    public static Item SOUL_FIRE;
     public static Item NETHER_PORTAL_BLOCK;
 
     private static Item registerBlockItem(Block block) {
-        return Registry.register(Registry.ITEM, new Identifier(Removed.MOD_ID, Registry.BLOCK.getId(block).getPath()), new BlockItem(block, new Item.Settings().group(Removed.MOD_ITEM_GROUP)));
+        return Registry.register(Registry.ITEM, new Identifier(Removed.MOD_ID, Registry.BLOCK.getId(block).getPath()), new BlockItem(block, new Item.Settings()));
     }
 
     public static void init() {
-        registerBlockItem(Blocks.WATER);
-        registerBlockItem(Blocks.LAVA);
-        registerBlockItem(Blocks.FIRE);
-        registerBlockItem(Blocks.SOUL_FIRE);
+        WATER = registerBlockItem(Blocks.WATER);
+        LAVA = registerBlockItem(Blocks.LAVA);
+        FIRE = registerBlockItem(Blocks.FIRE);
+        SOUL_FIRE = registerBlockItem(Blocks.SOUL_FIRE);
         NETHER_PORTAL_BLOCK = registerBlockItem(Blocks.NETHER_PORTAL);
-        registerBlockItem(Blocks.SPAWNER);
     }
 }
--- src/main/java/com/stygigoth/removed/Removed.java
+++ src/main/java/com/stygigoth/removed/Removed.java
@@ -13,10 +13,25 @@ public class Removed implements ModInitializer {
     public static final String MOD_ID = "removed";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final ItemGroup MOD_ITEM_GROUP = FabricItemGroupBuilder.build(
-            new Identifier(MOD_ID, MOD_ID),
+    public static final ItemGroup MOD_ITEM_GROUP = FabricItemGroupBuilder.create(
+            new Identifier(MOD_ID, MOD_ID)).icon(
             () -> new ItemStack(Blocks.BARRIER)
-    );
+    ).appendItems(
+            stacks -> {
+                stacks.add(new ItemStack(ModBlocks.WATER));
+                stacks.add(new ItemStack(ModBlocks.LAVA));
+                stacks.add(new ItemStack(ModBlocks.FIRE));
+                stacks.add(new ItemStack(ModBlocks.SOUL_FIRE));
+                stacks.add(new ItemStack(ModBlocks.NETHER_PORTAL_BLOCK));
+                stacks.add(new ItemStack(Blocks.SPAWNER));
+                stacks.add(new ItemStack(Blocks.COMMAND_BLOCK));
+                stacks.add(new ItemStack(Blocks.CHAIN_COMMAND_BLOCK));
+                stacks.add(new ItemStack(Blocks.REPEATING_COMMAND_BLOCK));
+                stacks.add(new ItemStack(Blocks.STRUCTURE_BLOCK));
+                stacks.add(new ItemStack(Blocks.JIGSAW));
+                stacks.add(new ItemStack(Blocks.STRUCTURE_VOID));
+            }
+    ).build();
 
     @Override
     public void onInitialize() {
